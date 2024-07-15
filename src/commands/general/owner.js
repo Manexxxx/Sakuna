080c80e3b58fab0abfa6c788ef74a2caf3b47e64
Gøjø satøru <manexx06@gmail.com>
2024-07-15 18:59:01 +0200

    Update owner.js

--- src/commands/general/owner.js
+++ src/commands/general/owner.js
@@ -5,7 +5,7 @@ module.exports = {
     react: "✅",
     description: 'Get information bot information',
     async execute(client, arg, M) {
-        let number = '263788671478@s.whatsapp.net'
+        let number = '263781512420@s.whatsapp.net'
         const owner = number
         let text = `Only @${owner.split('@')[0]} owned this bot { ${client.name} }!`
         await client.sendMessage(M.from , {text , mentions: [owner]} , {quoted: M})
