b1120f93815d1d6479457514011e72005841def8
Gøjø satøru <manexx06@gmail.com>
2024-07-15 18:50:44 +0200

    Update delete.js

--- src/commands/general/delete.js
+++ src/commands/general/delete.js
@@ -6,7 +6,7 @@ module.exports = {
     description: 'Deletes the quoted message',
     async execute(client, arg, M) {
         
-        if (!M.quoted) return M.reply('Quote the message that you want me to delete.')
+        if (!M.quoted) return M.reply('ooh hunny quote the message that you want me to delete for you.')
         await client.sendMessage(M.from, {
             delete: M.quoted.key
         })
